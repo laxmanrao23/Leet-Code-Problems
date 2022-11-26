1f083dfc758080b7eb9b3458447289f00c285931
Laxman Rao Savirigapu <109236954+laxmanrao23@users.noreply.github.com>
2022-11-26 12:47:28 +0530

    Time: 0 ms (100.00%), Space: 5.7 MB (95.11%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,32 @@
+#include <cmath>
+class Solution {
+public:
+    int mySqrt(int x) {
+        
+        int start = 0;
+        int end = x;
+        long long int mid = start + (end-start)/2;
+        long long int answer = -1;
+        
+        while(start<=end){
+            
+           long long int square = mid * mid;
+            
+            if(square == x){
+                return mid;
+            }
+            
+            if(square < x){
+             answer = mid;
+                start = mid + 1;
+            }
+            
+            else 
+            {
+                end = mid - 1;
+            }
+            mid = start + (end-start)/2;
+        }
+        return answer;
+    }
+};
